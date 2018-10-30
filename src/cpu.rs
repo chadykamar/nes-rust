cd805c49b69401a10b0e2db6c8decded88ad352b
Chady Kamar <chadykamar@users.noreply.github.com>
2018-10-30 02:25:14 -0400

    Add bitfield register

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,104 +1,211 @@
+use bitfield::{Bit, BitRange};
 
-/// Stack offset 
+/// Stack offset
 const STACK: u16 = 0x100;
 /// NMI vector
 const NMI_VECTOR: u16 = 0xFFFA;
 /// Reset vector
-const RESET_VECTOR: u16= 0xFFFC;
+const RESET_VECTOR: u16 = 0xFFFC;
 /// IRQ/BRK vector
 const IRQ_BRK_VECTOR: u16 = 0xFFFE;
 
+/// Ram Size
+const RAM_SIZE: usize = 0x800;
+
+const INSTRUCTION_CYCLES: [u8; 256] = [
+    7, 6, 2, 8, 3, 3, 5, 5, 3, 2, 2, 2, 4, 4, 6, 6, 2, 5, 2, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7,
+    6, 6, 2, 8, 3, 3, 5, 5, 4, 2, 2, 2, 4, 4, 6, 6, 2, 5, 2, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7,
+    6, 6, 2, 8, 3, 3, 5, 5, 3, 2, 2, 2, 3, 4, 6, 6, 2, 5, 2, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7,
+    6, 6, 2, 8, 3, 3, 5, 5, 4, 2, 2, 2, 5, 4, 6, 6, 2, 5, 2, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7,
+    2, 6, 2, 6, 3, 3, 3, 3, 2, 2, 2, 2, 4, 4, 4, 4, 2, 6, 2, 6, 4, 4, 4, 4, 2, 5, 2, 5, 5, 5, 5, 5,
+    2, 6, 2, 6, 3, 3, 3, 3, 2, 2, 2, 2, 4, 4, 4, 4, 2, 5, 2, 5, 4, 4, 4, 4, 2, 4, 2, 4, 4, 4, 4, 4,
+    2, 6, 2, 8, 3, 3, 5, 5, 2, 2, 2, 2, 4, 4, 6, 6, 2, 5, 2, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7,
+    2, 6, 2, 8, 3, 3, 5, 5, 2, 2, 2, 2, 4, 4, 6, 6, 2, 5, 2, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7,
+];
+
+enum Interrupt {
+    IRQ,
+    NMI,
+    Reset,
+    None,
+}
+
+bitfield!{
+    struct ProcessorStatus(u8);
+    impl Debug;
+    pub get_c, set_c: 0;
+    pub get_z, set_z: 1;
+    pub get_i, set_i: 2;
+    pub get_d, set_d: 3;
+    pub get_b, set_b: 4;
+    pub get_v, set_v: 6;
+    pub get_n, set_n: 7;
+}
+
 /// The CPU struct
 pub struct Cpu {
-    ram: [u8; 0x800],
-    reset: bool,
-    nmi: bool,
-    nmi_edge_detected: bool,
-    interrupt: bool,
+    ram: [u8; RAM_SIZE],
+    cycles: usize, // Cycles remaining
+    stall: usize,  // Cycles to stall the CPU for (for catch-up)
+    interrupt: Interrupt,
     // Registers
     pc: u16,
     sp: u8,
     a: u8,
     x: u8,
     y: u8,
-    status: u8, // The status register is made up of 5 flags and 3 unused bits
+    p: ProcessorStatus, // The status register is made up of 5 flags and 3 unused bits
 }
 
 enum AddressingMode {
-    Absolute(u16),
+    Absolute(Box<Fn() -> i16>),
 }
-
-
 
 impl Cpu {
     pub fn new() -> Cpu {
-        Cpu {
-            ram: [0; 0x800],
-            reset: true,
-            nmi: false,
-            nmi_edge_detected: false,
-            interrupt: false,
+        let mut cpu = Cpu {
+            ram: [0; RAM_SIZE],
+            cycles: 0,
+            stall: 0,
+            interrupt: Interrupt::None,
             pc: 0xC000,
             sp: 0xFD,
             a: 0,
             x: 0,
             y: 0,
-            status: 0x34
-        }
+            p: ProcessorStatus(0x24),
+        };
+        cpu.reset();
+        cpu
     }
 
     pub fn reset(&mut self) {
-        self.pc = 0x34;
+        self.p.set_bit_range(7, 0, 0x24);
         self.sp = 0xFD;
+        self.pc = self.read16(RESET_VECTOR)
+    }
+
+    pub fn trigger_nmi(&mut self) {
+        self.interrupt = Interrupt::NMI;
+    }
+
+    pub fn trigger_irq(&mut self) {
+        if self.p.get_i() {
+            self.interrupt = Interrupt::IRQ;
+        }
     }
 
     // Stack
 
     fn push(&mut self, val: u8) {
-        self.write(STACK | (self.sp as u16), val);
+        let sp = self.sp;
+        self.write(STACK | (sp as u16), val);
         self.sp -= 1;
-
     }
 
-    fn pop(&mut self) -> u8 {
+    fn pull(&mut self) -> u8 {
         self.sp += 1;
         self.read(STACK | (self.sp as u16))
+    }
 
+    fn pull16(&mut self) -> u16 {
+        let low = self.pull() as u16;
+        let high = self.pull() as u16;
+        (high << 8) | low
+    }
+
+    fn push16(&mut self, val: u16) {
+        let high = (val >> 8) as u8;
+        let low = (val & 0xFF) as u8;
+        self.push(high);
+        self.push(low);
     }
 
     /// Reads a byte from memory
     fn read(&self, addr: u16) -> u8 {
-        self.memory_access(addr, None)
+        // TODO match
+        if addr < 0x2000 {
+            self.ram[(addr & 0x7FF) as usize]
+        } else {
+            0
+        }
+    }
+
+    fn read16(&self, addr: u16) -> u16 {
+        let low = self.read(addr) as u16;
+        let high = self.read(addr + 1) as u16;
+        (high << 8) | low
+    }
+
+    fn read16_wrap(&self, addr: u16) -> u16 {
+        let low = self.read(addr) as u16;
+        let high = self.read(addr & 0xFF00) as u16;
+        (high << 8) | low
     }
 
     /// Writes a byte to memory
-    fn write(&self, addr: u16, val: u8) -> u8 {
-        self.memory_access(addr, Some(val))
-    }
-
-    /// Implements the CPU's memory map
-    fn memory_access(&self, addr: u16, val: Option<u8>) -> u8 {
-        // FIXME placeholder
-        0
-        // if addr < 0x2000 {
-        //     match val {
-        //         Some(val) => self.ram[addr & 0x7FF] = val
-        //         None => self.ram[addr & 0x7FF] 
-        // }
+    fn write(&mut self, addr: u16, val: u8) {
+        if addr < 0x2000 {
+            self.ram[(addr & 0x7FF) as usize] = val;
+        }
     }
 
     fn step(&mut self) {
+        if self.stall > 0 {
+            self.stall -= 1;
+        }
+
+        match self.interrupt {
+            Interrupt::IRQ => self.irq(),
+            Interrupt::NMI => self.nmi(),
+            Interrupt::Reset => self.reset(),
+            Interrupt::None => {}
+        }
+
         let opcode = self.read(self.pc);
+        let cycles = INSTRUCTION_CYCLES[opcode as usize];
         let op = self.decode(opcode);
         op();
         self.pc += 1;
+    }
 
-        self.reset = false;
+    fn resolve_addressing(mode: AddressingMode) -> u16 {
+        // match mode {}
+        return 0;
     }
 
     fn decode(&self, opcode: u8) -> Box<Fn()> {
         Box::new(move || println!("No!"))
     }
 
+    fn nmi(&mut self) {
+        let pc = self.pc;
+        self.push16(pc);
+        self.php();
+        self.pc = self.read16(NMI_VECTOR);
+        self.p.set_i(true);
+        self.cycles += 7;
+    }
 
+    fn irq(&mut self) {
+        let pc = self.pc;
+        self.push16(pc);
+        self.php();
+        self.pc = self.read16(IRQ_BRK_VECTOR);
+        self.p.set_i(true);
+        self.cycles += 7;
+    }
+
+    // Operations
+
+    fn adc(&self, addr: u16) {
+        let a = self.a;
+        let m = self.read(addr);
+    }
+
+    // PHP - Push Processor Status
+    fn php(&mut self) {
+        let p: u8 = self.p.bit_range(7, 0);
+        self.push(p | 0x10);
+    }
 }
