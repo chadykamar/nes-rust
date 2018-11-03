0c78370149f93c2c3c4b85c67511b4ac9ff9852e
Chady Kamar <chady.kamar@gmail.com>
2018-11-03 01:24:20 -0400

    Sprite struct and sprite eval

--- src/ppu.rs
+++ src/ppu.rs
@@ -19,13 +19,66 @@ const PALETTE: [u32; 64] = [
     0x000000,
 ];
 
-struct Sprite {
-    position: u8,
-    pattern: u8,
-    priority: u8,
-    index: u8
+// struct Sprite {
+//     position: u8,
+//     pattern: u8,
+//     priority: u8,
+//     index: u8,
+// }
+
+bitfield!{
+    /// Byte 2 of a `Sprite`
+    /// Bits 2, 3 and 4 are unimplemented.
+    #[derive(Copy, Clone)]
+    struct SpriteAttributes(u8);
+    impl Debug;
+    /// Palette (4 to 7) of sprite
+    pub palette, set_palette: 1, 0;
+    /// Priority (0: in front of background; 1: behind background)
+    pub priority, set_priority: 5;
+    /// Flip sprite horizontally.
+    /// Flipping does not change the position of the sprite's bounding box, just
+    /// the position of pixels within the sprite. If, for example, a sprite co-
+    /// vers (120, 130) through (127, 137), it'll still cover the same area
+    /// when flipped.
+    pub flip_h, set_flip_h: 3;
+    /// Flip sprite vertically.
+    /// * In 8x16 mode, vertical flip flips each of the subtiles and also exchan-
+    /// ges their position; the odd-numbered tile of a vertically flipped sprite
+    /// is drawn on top.
+    /// * Flipping does not change the position of the sprite's bounding box, just
+    /// the position of pixels within the sprite. If, for example, a sprite
+    /// covers (120, 130) through (127, 137), it'll still cover the same area
+    /// when flipped.
+    pub flip_v, set_flip_v: 4;
 }
 
+#[derive(Copy, Clone)]
+struct Sprite {
+    /// Y position of top of sprite.
+    /// Sprite data is delayed by one scanline; you must subtract 1 from
+    /// the sprite's Y coordinate before writing it here. Hide a sprite by
+    /// writing any values in $EF-$FF here. Sprites are never displayed on the
+    /// first line of the picture, and it is impossible to place a sprite
+    /// partially off the top of the screen.
+    y: u8,
+    /// Tile index number.
+    /// * For 8x8 sprites, this is the tile number of this sprite within the
+    /// pattern table selected in bit 3 of PPUCTRL ($2000).
+    /// * For 8x16 sprites, the PPU ignores the pattern table selection and
+    /// selects a pattern table from bit 0 of this number.
+    tile: u8,
+    /// X position of left side of sprite.
+    /// X-scroll values of $F9-FF results in parts of the sprite to be past the
+    /// right edge of the screen, thus invisible. It is not possible to have a
+    /// sprite partially visible on the left edge. Instead, left-clipping
+    /// through PPUMASK ($2001) can be used to simulate this effect.
+    x: u8,
+    /// Sprite attributes, including palette priority, and flipping.
+    attributes: SpriteAttributes,
+}
+
+///
 bitfield!{
     struct PpuCtrl(u8);
     impl Debug;
@@ -38,6 +91,7 @@ bitfield!{
     pub nmi_vblank, set_n: 7;
 }
 
+/// The
 bitfield!{
     struct PpuMask(u8);
     impl Debug;
@@ -52,6 +106,7 @@ bitfield!{
 }
 
 bitfield!{
+    /// The PPUSTATUS Register implementation.
     struct PpuStatus(u8);
     impl Debug;
     /// Least significant bits previously written into a PPU register
@@ -76,7 +131,7 @@ bitfield!{
     pub vblank_started, set_vblank_started: 7;
 }
 
-pub struct Ppu {
+pub struct Ppu<'a> {
     scanline: usize,
     cycle: usize,
     frame: usize,
@@ -84,13 +139,30 @@ pub struct Ppu {
     vram_addr: u16,
     temp_vram_addr: u16,
 
-    // Mem
+    // Mem'
+    /// Object Attribute Memory which contains a display list of up to 64
+    /// sprites, where each sprite's information occupies 4 bytes.
+    oam: [&'a Sprite; 64],
+
+    // The NES uses two palettes, each with 16 entries, the image palette ($3F00-$3F0F) and the
+    // sprite palette ($3F10-$3F1F). Since only 64 unique values are needed,
+    // bits 6 and 7 can be ignored.
+    /// The image palette shows colors available for background tiles.
+    /// It does not store the actual color values, only the index for the color
+    /// in the system palette `PALETTE`.
+    image_palette: [u8; 16],
+    /// The sprite palette shows the colours currently available for sprites.
+    /// It does not store the actual color values, only the index for the color
+    /// in the system palette `PALETTE`.
+    sprite_palette: [u8; 16],
 
     // palette: [u8; 32],
     // nametable: [u8; 0x800],
-    sprites: Vec<Sprite>,
+    sprites: Vec<&'a Sprite>,
 
     // Flags
+    /// Write toggle.
+    write: bool,
     /// Even (true) or odd (false)
     even: bool,
 
@@ -100,7 +172,6 @@ pub struct Ppu {
     low_tile: u8,
     high_tile: u8,
     tile_data: usize,
-
 
     //Registers
     /// $2000 PPUCTRL
@@ -127,13 +198,24 @@ pub struct Ppu {
     // nametable
 }
 
-impl Ppu {
-    pub fn new() -> Ppu {
+impl<'a> Ppu<'a> {
+    pub fn new() -> Ppu<'static> {
         Ppu {
             scanline: 0,
             cycle: 0,
             frame: 0,
             screen: Array3::zeros((256, 240, 3)),
+
+            image_palette: [0; 16],
+            sprite_palette: [0; 16],
+
+            // TODO initialize properly
+            oam: [&Sprite {
+                y: 0,
+                tile: 0,
+                x: 0,
+                attributes: SpriteAttributes(0),
+            }; 64],
             nametable: 0,
             vram_addr: 0,
             temp_vram_addr: 0,
@@ -141,9 +223,11 @@ impl Ppu {
             low_tile: 0,
             high_tile: 0,
             tile_data: 0,
-            sprites: vec!(),
+            sprites: vec![],
 
             even: true,
+            write: true,
+
             ppu_ctrl: PpuCtrl(0),
             ppu_mask: PpuMask(0),
             ppu_status: PpuStatus(0),
@@ -214,8 +298,34 @@ impl Ppu {
         }
     }
 
-    fn eval(&self) {
+    /// PPU sprite evaluation is an operation done by the PPU once each
+    /// scanline. It prepares the set of sprites and fetches their data to be
+    /// rendered on the next scanline. Each scanline, the PPU reads the
+    /// spritelist (that is, Object Attribute Memory) to see which to draw:
+    /// * First, it clears the list of sprites to draw.
+    /// * Second, it reads through OAM, checking which sprites will be on this
+    /// scanline. It chooses the first eight it finds that do.
+    /// * Third, if eight sprites were found, it checks
+    /// (in a wrongly-implemented fashion) for further sprites on the scanline
+    /// to see if the sprite overflow flag should be set.
+    /// * Fourth, using the details for the eight (or fewer) sprites chosen,
+    /// it determines which pixels each has on the scanline and where to draw them.
+    fn evaluate_sprites(&mut self) {
+        let height = if self.ppu_ctrl.sprite_size() { 8 } else { 16 };
+        self.sprites = vec![];
 
+        for sprite in self.oam.iter() {
+            let row = self.scanline - sprite.y as usize;
+            if row >= height {
+                continue;
+            }
+            if self.sprites.len() < 8 {
+                self.sprites.push(sprite);
+            }
+        }
+        if self.sprites.len() > 8 {
+            self.ppu_status.set_sprite_overflow(true);
+        }
     }
 
     fn render_pixel(&mut self) {}
@@ -312,13 +422,13 @@ impl Ppu {
 
         if self.ppu_mask.show_sprites() {
             if self.cycle == 257 {
-                // if visible_line {
-                //     self.eval();
-                // } else {
-                //     // Sprites
-                    
-                // }
-		}
+                if visible_line {
+                    self.evaluate_sprites();
+                } else {
+                    // Sprites
+
+                }
+            }
         }
 
         // vblank
